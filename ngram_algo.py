5b41afffc0458a1f973d14cd7cfbd2ca75939c65
PCK <charles.kantane@outlook.fr>
2023-04-06 00:23:02 +0200

    Added verification conditions on the attribute N

--- ngram_algo.py
+++ ngram_algo.py
@@ -10,33 +10,38 @@ class NGram:
         self.tokenizedSentence = self.token.tokenize()[0]
 
     def ngram(self):
-        tuplesList = []
-        for list in self.tokenizedSentence:
-            sublist = []
-            for w in list:
-                subsublist = []
-                for i in range(self.N + 1):
-                    try:
-                        subsublist.append(list[list.index(w) + i])
-                    except(IndexError):
-                        pass
+        if isinstance(self.N, int):
+            if self.N < len(self.tokenizedSentence):
+                tuplesList = []
+                for list in self.tokenizedSentence:
+                    sublist = []
+                    for w in list:
+                        subsublist = []
+                        for i in range(self.N + 1):
+                            try:
+                                subsublist.append(list[list.index(w) + i])
+                            except(IndexError):
+                                pass
 
-                sublist.append(tuple(subsublist))
+                        sublist.append(tuple(subsublist))
 
-            for w in reversed(list):
-                subsublist = []
-                if list.index(w) != 0:
-                    for i in range(self.N + 1):
-                        try:
-                            subsublist.append(list[list.index(w) - i])
-                        except(IndexError):
-                            pass
+                    for w in reversed(list):
+                        subsublist = []
+                        if list.index(w) != 0:
+                            for i in range(self.N + 1):
+                                try:
+                                    subsublist.append(list[list.index(w) - i])
+                                except(IndexError):
+                                    pass
 
-                    sublist.append(tuple(subsublist))
+                            sublist.append(tuple(subsublist))
+                    tuplesList.append(sublist)
+                return tuplesList
 
-            tuplesList.append(sublist)
-
-        return tuplesList
+            else:
+                print('You choose a greater number than the length of the list. Please choose a smaller number.')
+        else:
+            print('The attribute N is not a number. Please enter a number.')
 
     def OneHotEncoding(self):
         dicoEncoding = {}
@@ -55,9 +60,10 @@ class NGram:
 text = "Bootstrap est une collection d'outils utiles à la création du design de sites et d'applications web. C'est un ensemble qui contient des codes HTML et CSS, des formulaires, boutons, outils de navigation et autres éléments interactifs, ainsi que des extensions JavaScript en option."
 # text2 = "With administrative worries attended to, Wilsdorf turned the company's attention to a marketing challenge: the infiltration of dust and moisture under the dial and crown, which damaged the movement. To address this problem, in 1926 a third-party casemaker produced a waterproof and dustproof wristwatch for Rolex, giving it the name 'Oyster'."
 ngram = NGram(sentence=text)  # By default, N = 1 and language = 'french'
-getTuplesList = ngram.ngram()
-ohe = ngram.OneHotEncoding()
-
 print(ngram.tokenizedSentence)
+
+getTuplesList = ngram.ngram()
 print(getTuplesList)
+
+ohe = ngram.OneHotEncoding()
 print(ohe)
